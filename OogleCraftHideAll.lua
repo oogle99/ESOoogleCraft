9a4315d56d753c3dec13de6a66c990d197850a2b
oogle99 <158149418+oogle99@users.noreply.github.com>
2024-03-06 21:57:32 -0800

    Update OogleCraftHideAll.lua
    Condensed.

--- OogleCraftHideAll.lua
+++ OogleCraftHideAll.lua
@@ -4,179 +4,193 @@ local windowIsHidden = false
 function OogleCraftHideAll.toggleWindow()
     if windowIsHidden == false then
         windowIsHidden = true
-        OogleCraftMainPanel:SetHidden(true)
-     
-        OogleCraftLvlDropWin:SetHidden(true)
-        OogleCraftSet1ListDropWin:SetHidden(true)
-        OogleCraftSet2ListDropWin:SetHidden(true)
-        OogleCraftSet3ListDropWin:SetHidden(true)
 
-        OogleCraftArmorSetQualityDropWin:SetHidden(true)
+        local elementsToHide = {
+            OogleCraftMainPanel,
 
-        OogleCraftArmorSetShieldDropWin:SetHidden(true)
-        OogleCraftArmorSetChestDropWin:SetHidden(true)
-        OogleCraftArmorSetFeetDropWin:SetHidden(true)
-        OogleCraftArmorSetHandsDropWin:SetHidden(true)
-        OogleCraftArmorSetLegsDropWin:SetHidden(true)
-        OogleCraftArmorSetWaistDropWin:SetHidden(true)
-        OogleCraftArmorSetHeadDropWin:SetHidden(true)
-        OogleCraftArmorSetShouldersDropWin:SetHidden(true)
+            OogleCraftLvlDropWin,
+            OogleCraftSet1ListDropWin,
+            OogleCraftSet2ListDropWin,
+            OogleCraftSet3ListDropWin,
 
-        OogleCraftArmorWeightChestDropWin:SetHidden(true)
-        OogleCraftArmorWeightFeetDropWin:SetHidden(true)
-        OogleCraftArmorWeightHandsDropWin:SetHidden(true)
-        OogleCraftArmorWeightLegsDropWin:SetHidden(true)
-        OogleCraftArmorWeightWaistDropWin:SetHidden(true)
-        OogleCraftArmorWeightHeadDropWin:SetHidden(true)
-        OogleCraftArmorWeightShouldersDropWin:SetHidden(true)
+            OogleCraftArmorSetQualityDropWin,
 
-        OogleCraftArmorTraitShieldDropWin:SetHidden(true)
-        OogleCraftArmorTraitChestDropWin:SetHidden(true)
-        OogleCraftArmorTraitFeetDropWin:SetHidden(true)
-        OogleCraftArmorTraitHandsDropWin:SetHidden(true)
-        OogleCraftArmorTraitLegsDropWin:SetHidden(true)
-        OogleCraftArmorTraitWaistDropWin:SetHidden(true)
-        OogleCraftArmorTraitHeadDropWin:SetHidden(true)
-        OogleCraftArmorTraitShouldersDropWin:SetHidden(true)
+            OogleCraftArmorSetShieldDropWin,
+            OogleCraftArmorSetChestDropWin,
+            OogleCraftArmorSetFeetDropWin,
+            OogleCraftArmorSetHandsDropWin,
+            OogleCraftArmorSetLegsDropWin,
+            OogleCraftArmorSetWaistDropWin,
+            OogleCraftArmorSetHeadDropWin,
+            OogleCraftArmorSetShouldersDropWin,
 
-        OogleCraftArmorEnchantQualityDropWin:SetHidden(true)
+            OogleCraftArmorWeightChestDropWin,
+            OogleCraftArmorWeightFeetDropWin,
+            OogleCraftArmorWeightHandsDropWin,
+            OogleCraftArmorWeightLegsDropWin,
+            OogleCraftArmorWeightWaistDropWin,
+            OogleCraftArmorWeightHeadDropWin,
+            OogleCraftArmorWeightShouldersDropWin,
 
-        OogleCraftArmorEnchantShieldDropWin:SetHidden(true)
-        OogleCraftArmorEnchantChestDropWin:SetHidden(true)
-        OogleCraftArmorEnchantFeetDropWin:SetHidden(true)
-        OogleCraftArmorEnchantHandsDropWin:SetHidden(true)
-        OogleCraftArmorEnchantLegsDropWin:SetHidden(true)
-        OogleCraftArmorEnchantWaistDropWin:SetHidden(true)
-        OogleCraftArmorEnchantHeadDropWin:SetHidden(true)
-        OogleCraftArmorEnchantShouldersDropWin:SetHidden(true)
+            OogleCraftArmorTraitShieldDropWin,
+            OogleCraftArmorTraitChestDropWin,
+            OogleCraftArmorTraitFeetDropWin,
+            OogleCraftArmorTraitHandsDropWin,
+            OogleCraftArmorTraitLegsDropWin,
+            OogleCraftArmorTraitWaistDropWin,
+            OogleCraftArmorTraitHeadDropWin,
+            OogleCraftArmorTraitShouldersDropWin,
 
-        OogleCraftJewelrySetQualityDropWin:SetHidden(true)
+            OogleCraftArmorEnchantQualityDropWin,
 
-        OogleCraftJewelrySetRing1DropWin:SetHidden(true)
-        OogleCraftJewelrySetRing2DropWin:SetHidden(true)
-        OogleCraftJewelrySetNecklaceDropWin:SetHidden(true)
+            OogleCraftArmorEnchantShieldDropWin,
+            OogleCraftArmorEnchantChestDropWin,
+            OogleCraftArmorEnchantFeetDropWin,
+            OogleCraftArmorEnchantHandsDropWin,
+            OogleCraftArmorEnchantLegsDropWin,
+            OogleCraftArmorEnchantWaistDropWin,
+            OogleCraftArmorEnchantHeadDropWin,
+            OogleCraftArmorEnchantShouldersDropWin,
 
-        OogleCraftJewelryTraitRing1DropWin:SetHidden(true)
-        OogleCraftJewelryTraitRing2DropWin:SetHidden(true)
-        OogleCraftJewelryTraitNecklaceDropWin:SetHidden(true)
+            OogleCraftJewelrySetQualityDropWin,
 
-        OogleCraftJewelryEnchantQualityDropWin:SetHidden(true)
+            OogleCraftJewelrySetRing1DropWin,
+            OogleCraftJewelrySetRing2DropWin,
+            OogleCraftJewelrySetNecklaceDropWin,
 
-        OogleCraftJewelryEnchantRing1DropWin:SetHidden(true)
-        OogleCraftJewelryEnchantRing2DropWin:SetHidden(true)
-        OogleCraftJewelryEnchantNecklaceDropWin:SetHidden(true)
+            OogleCraftJewelryTraitRing1DropWin,
+            OogleCraftJewelryTraitRing2DropWin,
+            OogleCraftJewelryTraitNecklaceDropWin,
 
-        OogleCraftWeaponSetQualityDropWin:SetHidden(true)
+            OogleCraftJewelryEnchantQualityDropWin,
 
-        OogleCraftWeaponSetFrontBar1DropWin:SetHidden(true)
-        OogleCraftWeaponSetFrontBar2DropWin:SetHidden(true)
-        OogleCraftWeaponSetBackBar1DropWin:SetHidden(true)
-        OogleCraftWeaponSetBackBar2DropWin:SetHidden(true)
+            OogleCraftJewelryEnchantRing1DropWin,
+            OogleCraftJewelryEnchantRing2DropWin,
+            OogleCraftJewelryEnchantNecklaceDropWin,
 
-        OogleCraftWeaponTypeFrontBar1DropWin:SetHidden(true)
-        OogleCraftWeaponTypeFrontBar2DropWin:SetHidden(true)
-        OogleCraftWeaponTypeBackBar1DropWin:SetHidden(true)
-        OogleCraftWeaponTypeBackBar2DropWin:SetHidden(true)
+            OogleCraftWeaponSetQualityDropWin,
 
-        OogleCraftWeaponTraitFrontBar1DropWin:SetHidden(true)
-        OogleCraftWeaponTraitFrontBar2DropWin:SetHidden(true)
-        OogleCraftWeaponTraitBackBar1DropWin:SetHidden(true)
-        OogleCraftWeaponTraitBackBar2DropWin:SetHidden(true)
+            OogleCraftWeaponSetFrontBar1DropWin,
+            OogleCraftWeaponSetFrontBar2DropWin,
+            OogleCraftWeaponSetBackBar1DropWin,
+            OogleCraftWeaponSetBackBar2DropWin,
 
-        OogleCraftWeaponEnchantQualityDropWin:SetHidden(true)
+            OogleCraftWeaponTypeFrontBar1DropWin,
+            OogleCraftWeaponTypeFrontBar2DropWin,
+            OogleCraftWeaponTypeBackBar1DropWin,
+            OogleCraftWeaponTypeBackBar2DropWin,
 
-        OogleCraftWeaponEnchantFrontBar1DropWin:SetHidden(true)
-        OogleCraftWeaponEnchantFrontBar2DropWin:SetHidden(true)
-        OogleCraftWeaponEnchantBackBar1DropWin:SetHidden(true)
-        OogleCraftWeaponEnchantBackBar2DropWin:SetHidden(true)
+            OogleCraftWeaponTraitFrontBar1DropWin,
+            OogleCraftWeaponTraitFrontBar2DropWin,
+            OogleCraftWeaponTraitBackBar1DropWin,
+            OogleCraftWeaponTraitBackBar2DropWin,
+
+            OogleCraftWeaponEnchantQualityDropWin,
+
+            OogleCraftWeaponEnchantFrontBar1DropWin,
+            OogleCraftWeaponEnchantFrontBar2DropWin,
+            OogleCraftWeaponEnchantBackBar1DropWin,
+            OogleCraftWeaponEnchantBackBar2DropWin
+        }
+
+        for _, element in ipairs(elementsToHide) do
+            element:SetHidden(true)
+        end
 
     else
         windowIsHidden = false
-        OogleCraftMainPanel:SetHidden(false)
 
-        OogleCraftLvlDropWin:SetHidden(false)
-        OogleCraftSet1ListDropWin:SetHidden(false)
-        OogleCraftSet2ListDropWin:SetHidden(false)
-        OogleCraftSet3ListDropWin:SetHidden(false)
+            local elementsToUnHide = {
+            OogleCraftMainPanel,
 
-        OogleCraftArmorSetQualityDropWin:SetHidden(false)
+            OogleCraftLvlDropWin,
+            OogleCraftSet1ListDropWin,
+            OogleCraftSet2ListDropWin,
+            OogleCraftSet3ListDropWin,
 
-        OogleCraftArmorSetShieldDropWin:SetHidden(false)
-        OogleCraftArmorSetChestDropWin:SetHidden(false)
-        OogleCraftArmorSetFeetDropWin:SetHidden(false)
-        OogleCraftArmorSetHandsDropWin:SetHidden(false)
-        OogleCraftArmorSetLegsDropWin:SetHidden(false)
-        OogleCraftArmorSetWaistDropWin:SetHidden(false)
-        OogleCraftArmorSetHeadDropWin:SetHidden(false)
-        OogleCraftArmorSetShouldersDropWin:SetHidden(false)
+            OogleCraftArmorSetQualityDropWin,
 
-        OogleCraftArmorWeightChestDropWin:SetHidden(false)
-        OogleCraftArmorWeightFeetDropWin:SetHidden(false)
-        OogleCraftArmorWeightHandsDropWin:SetHidden(false)
-        OogleCraftArmorWeightLegsDropWin:SetHidden(false)
-        OogleCraftArmorWeightWaistDropWin:SetHidden(false)
-        OogleCraftArmorWeightHeadDropWin:SetHidden(false)
-        OogleCraftArmorWeightShouldersDropWin:SetHidden(false)
+            OogleCraftArmorSetShieldDropWin,
+            OogleCraftArmorSetChestDropWin,
+            OogleCraftArmorSetFeetDropWin,
+            OogleCraftArmorSetHandsDropWin,
+            OogleCraftArmorSetLegsDropWin,
+            OogleCraftArmorSetWaistDropWin,
+            OogleCraftArmorSetHeadDropWin,
+            OogleCraftArmorSetShouldersDropWin,
 
-        OogleCraftArmorTraitShieldDropWin:SetHidden(false)
-        OogleCraftArmorTraitChestDropWin:SetHidden(false)
-        OogleCraftArmorTraitFeetDropWin:SetHidden(false)
-        OogleCraftArmorTraitHandsDropWin:SetHidden(false)
-        OogleCraftArmorTraitLegsDropWin:SetHidden(false)
-        OogleCraftArmorTraitWaistDropWin:SetHidden(false)
-        OogleCraftArmorTraitHeadDropWin:SetHidden(false)
-        OogleCraftArmorTraitShouldersDropWin:SetHidden(false)
+            OogleCraftArmorWeightChestDropWin,
+            OogleCraftArmorWeightFeetDropWin,
+            OogleCraftArmorWeightHandsDropWin,
+            OogleCraftArmorWeightLegsDropWin,
+            OogleCraftArmorWeightWaistDropWin,
+            OogleCraftArmorWeightHeadDropWin,
+            OogleCraftArmorWeightShouldersDropWin,
 
-        OogleCraftArmorEnchantQualityDropWin:SetHidden(false)
+            OogleCraftArmorTraitShieldDropWin,
+            OogleCraftArmorTraitChestDropWin,
+            OogleCraftArmorTraitFeetDropWin,
+            OogleCraftArmorTraitHandsDropWin,
+            OogleCraftArmorTraitLegsDropWin,
+            OogleCraftArmorTraitWaistDropWin,
+            OogleCraftArmorTraitHeadDropWin,
+            OogleCraftArmorTraitShouldersDropWin,
 
-        OogleCraftArmorEnchantShieldDropWin:SetHidden(false)
-        OogleCraftArmorEnchantChestDropWin:SetHidden(false)
-        OogleCraftArmorEnchantFeetDropWin:SetHidden(false)
-        OogleCraftArmorEnchantHandsDropWin:SetHidden(false)
-        OogleCraftArmorEnchantLegsDropWin:SetHidden(false)
-        OogleCraftArmorEnchantWaistDropWin:SetHidden(false)
-        OogleCraftArmorEnchantHeadDropWin:SetHidden(false)
-        OogleCraftArmorEnchantShouldersDropWin:SetHidden(false)
+            OogleCraftArmorEnchantQualityDropWin,
 
-        OogleCraftJewelrySetQualityDropWin:SetHidden(false)
+            OogleCraftArmorEnchantShieldDropWin,
+            OogleCraftArmorEnchantChestDropWin,
+            OogleCraftArmorEnchantFeetDropWin,
+            OogleCraftArmorEnchantHandsDropWin,
+            OogleCraftArmorEnchantLegsDropWin,
+            OogleCraftArmorEnchantWaistDropWin,
+            OogleCraftArmorEnchantHeadDropWin,
+            OogleCraftArmorEnchantShouldersDropWin,
 
-        OogleCraftJewelrySetRing1DropWin:SetHidden(false)
-        OogleCraftJewelrySetRing2DropWin:SetHidden(false)
-        OogleCraftJewelrySetNecklaceDropWin:SetHidden(false)
+            OogleCraftJewelrySetQualityDropWin,
 
-        OogleCraftJewelryTraitRing1DropWin:SetHidden(false)
-        OogleCraftJewelryTraitRing2DropWin:SetHidden(false)
-        OogleCraftJewelryTraitNecklaceDropWin:SetHidden(false)
+            OogleCraftJewelrySetRing1DropWin,
+            OogleCraftJewelrySetRing2DropWin,
+            OogleCraftJewelrySetNecklaceDropWin,
 
-        OogleCraftJewelryEnchantQualityDropWin:SetHidden(false)
+            OogleCraftJewelryTraitRing1DropWin,
+            OogleCraftJewelryTraitRing2DropWin,
+            OogleCraftJewelryTraitNecklaceDropWin,
 
-        OogleCraftJewelryEnchantRing1DropWin:SetHidden(false)
-        OogleCraftJewelryEnchantRing2DropWin:SetHidden(false)
-        OogleCraftJewelryEnchantNecklaceDropWin:SetHidden(false)
+            OogleCraftJewelryEnchantQualityDropWin,
 
-        OogleCraftWeaponSetQualityDropWin:SetHidden(false)
+            OogleCraftJewelryEnchantRing1DropWin,
+            OogleCraftJewelryEnchantRing2DropWin,
+            OogleCraftJewelryEnchantNecklaceDropWin,
 
-        OogleCraftWeaponSetFrontBar1DropWin:SetHidden(false)
-        OogleCraftWeaponSetFrontBar2DropWin:SetHidden(false)
-        OogleCraftWeaponSetBackBar1DropWin:SetHidden(false)
-        OogleCraftWeaponSetBackBar2DropWin:SetHidden(false)
+            OogleCraftWeaponSetQualityDropWin,
 
-        OogleCraftWeaponTypeFrontBar1DropWin:SetHidden(false)
-        OogleCraftWeaponTypeFrontBar2DropWin:SetHidden(false)
-        OogleCraftWeaponTypeBackBar1DropWin:SetHidden(false)
-        OogleCraftWeaponTypeBackBar2DropWin:SetHidden(false)
+            OogleCraftWeaponSetFrontBar1DropWin,
+            OogleCraftWeaponSetFrontBar2DropWin,
+            OogleCraftWeaponSetBackBar1DropWin,
+            OogleCraftWeaponSetBackBar2DropWin,
 
-        OogleCraftWeaponTraitFrontBar1DropWin:SetHidden(false)
-        OogleCraftWeaponTraitFrontBar2DropWin:SetHidden(false)
-        OogleCraftWeaponTraitBackBar1DropWin:SetHidden(false)
-        OogleCraftWeaponTraitBackBar2DropWin:SetHidden(false)
+            OogleCraftWeaponTypeFrontBar1DropWin,
+            OogleCraftWeaponTypeFrontBar2DropWin,
+            OogleCraftWeaponTypeBackBar1DropWin,
+            OogleCraftWeaponTypeBackBar2DropWin,
 
-        OogleCraftWeaponEnchantQualityDropWin:SetHidden(false)
+            OogleCraftWeaponTraitFrontBar1DropWin,
+            OogleCraftWeaponTraitFrontBar2DropWin,
+            OogleCraftWeaponTraitBackBar1DropWin,
+            OogleCraftWeaponTraitBackBar2DropWin,
 
-        OogleCraftWeaponEnchantFrontBar1DropWin:SetHidden(false)
-        OogleCraftWeaponEnchantFrontBar2DropWin:SetHidden(false)
-        OogleCraftWeaponEnchantBackBar1DropWin:SetHidden(false)
-        OogleCraftWeaponEnchantBackBar2DropWin:SetHidden(false)
+            OogleCraftWeaponEnchantQualityDropWin,
+
+            OogleCraftWeaponEnchantFrontBar1DropWin,
+            OogleCraftWeaponEnchantFrontBar2DropWin,
+            OogleCraftWeaponEnchantBackBar1DropWin,
+            OogleCraftWeaponEnchantBackBar2DropWin
+        }
+
+        for _, element in ipairs(elementsToUnHide) do
+            element:SetHidden(false)
+        end
 
     end
 end
