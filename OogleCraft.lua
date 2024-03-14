6055453d1144dc202336066c301571b258023dbf
oogle99 <158149418+oogle99@users.noreply.github.com>
2024-03-14 00:51:46 -0700

    Attempting to do something with the buttons and dropdowns

--- OogleCraft.lua
+++ OogleCraft.lua
@@ -7,8 +7,6 @@ end
 
 function OogleCraft.Initialize()
 
-    
-
 end
 
 function OogleCraft.OnAddOnLoaded(event, addonName)
@@ -19,6 +17,30 @@ function OogleCraft.OnAddOnLoaded(event, addonName)
     end
 end
 
-
-
 EVENT_MANAGER:RegisterForEvent(OogleCraft.name, EVENT_ADD_ON_LOADED, OogleCraft.OnAddOnLoaded)
+
+function OogleCraft.onItemSelectFunctionality(oogleCraftDropdownFrameName, choiceText)
+    --d(oogleCraftDropdownFrameName, choiceText)
+
+
+OogleCraftDropdownChoice = OogleCraftConstantDropdownChoices[oogleCraftDropdownFrameName]
+
+    if OogleCraftDropdownChoice then
+
+        if OogleCraftDropdownChoice[choiceText] then
+            d(OogleCraftDropdownChoice[choiceText])
+
+        else
+            d("Me thinks mehaps messed up somewhere.")
+        end
+    end
+end
+
+
+function OogleCraft.addToQueueFunctionality(oogleCraftButtonFrameName)
+    local setQualityChoices = OogleCraftConstantDropdownChoices["ArmorSetQuality"]
+    d(oogleCraftButtonFrameName)
+    if oogleCraftButtonFrameName == "ShieldQueue" then
+        d(setQualityChoices["White"])
+    end
+end
