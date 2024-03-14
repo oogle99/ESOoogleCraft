e5b9ae84e020a6821b6d56d5be3254bb10d42410
oogle99 <158149418+oogle99@users.noreply.github.com>
2024-03-14 00:50:24 -0700

    Changed dropdown creation to make hiding easier

--- OogleCraftWeaponDropUI.lua
+++ OogleCraftWeaponDropUI.lua
@@ -3,20 +3,19 @@ OogleCraft.name = "OogleCraft"
 
 local wm = WINDOW_MANAGER
 
-local function createDropdown(frameName, choices, xOffset, yOffset, dropWide)
-    local frame = wm:CreateTopLevelWindow("OogleCraftWeapon" .. frameName .. "DropWin")
-    frame:SetAnchor(TOPLEFT)
-    frame:SetDimensions(125, 50)
-
-    local dropdownContainer = wm:CreateControlFromVirtual("OogleCraftWeapon" .. frameName .. "Drop", frame, "ScrollCombobox")
-    dropdownContainer:SetAnchor(TOPLEFT, OogleCraftMainPanel, TOPLEFT, xOffset + 7, yOffset) -- y180
+local function createDropdown(oogleCraftDropdownFrameName, choices, xOffset, yOffset, dropWide)
+    local dropdownContainer = wm:CreateControlFromVirtual("OogleCraftWeapon" .. oogleCraftDropdownFrameName .. "Drop", OogleCraftMainPanel, "ScrollCombobox")
+    dropdownContainer:SetAnchor(TOPLEFT, OogleCraftMainPanel, TOPLEFT, xOffset + 7, yOffset)
+    dropdownContainer:SetDimensions(125, 50)
     dropdownContainer:GetNamedChild("Dropdown"):SetWidth(dropWide)
 
     local dropdown = dropdownContainer.dropdown
     dropdown:SetSelectedItem(choices[1])
 
+    dropdown:SetSortsItems(false)
+
     local function OnItemSelect(_, choiceText, choice)
-        d(frameName, choiceText)
+        OogleCraft.onItemSelectFunctionality(oogleCraftDropdownFrameName, choiceText)
     end
 
     for i=1,#choices do
@@ -31,7 +30,7 @@ end
 
 local OogleCraftWeaponSetQualityChoices = {"[Weapon Quality]", "White", "Green", "Blue", "Purple", "Yellow", "Orange"}
 
-createDropdown("SetQuality", OogleCraftWeaponSetQualityChoices, 1375, 155, 500)
+createDropdown("WeaponSetQuality", OogleCraftWeaponSetQualityChoices, 1375, 145, 500)
 
 ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 --- Weapon Set Choices -------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -42,10 +41,10 @@ local OogleCraftWeaponSetFrontBar2Choices = {"[Front Bar #1]", "Set #1", "Set #2
 local OogleCraftWeaponSetBackBar1Choices = {"[Front Bar #1]", "Set #1", "Set #2", "Set #3"}
 local OogleCraftWeaponSetBackBar2Choices = {"[Front Bar #1]", "Set #1", "Set #2", "Set #3"}
 
-createDropdown("SetFrontBar1", OogleCraftWeaponSetFrontBar1Choices, 1375, 180, 125)
-createDropdown("SetFrontBar2", OogleCraftWeaponSetFrontBar2Choices, 1500, 180, 125)
-createDropdown("SetBackBar1", OogleCraftWeaponSetBackBar1Choices, 1625, 180, 125)
-createDropdown("SetBackBar2", OogleCraftWeaponSetBackBar2Choices, 1750, 180, 125)
+createDropdown("SetFrontBar1", OogleCraftWeaponSetFrontBar1Choices, 1375, 170, 125)
+createDropdown("SetFrontBar2", OogleCraftWeaponSetFrontBar2Choices, 1500, 170, 125)
+createDropdown("SetBackBar1", OogleCraftWeaponSetBackBar1Choices, 1625, 170, 125)
+createDropdown("SetBackBar2", OogleCraftWeaponSetBackBar2Choices, 1750, 170, 125)
 
 ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 --- Weapon Type Choices ------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -56,10 +55,10 @@ local OogleCraftWeaponTypeFrontBar2Choices = {"[Front Bar #2]", "Axe", "Mace", "
 local OogleCraftWeaponTypeBackBar1Choices = {"[Back Bar #1]", "Axe", "Mace", "Sword", "Battle Axe", "Maul", "Greatsword", "Dagger", "Bow", "Inferno", "Ice", "Lightning", "Restoration"}
 local OogleCraftWeaponTypeBackBar2Choices = {"[Back Bar #2]", "Axe", "Mace", "Sword", "Battle Axe", "Maul", "Greatsword", "Dagger", "Bow", "Inferno", "Ice", "Lightning", "Restoration"}
 
-createDropdown("TypeFrontBar1", OogleCraftWeaponTypeFrontBar1Choices, 1375, 255, 125)
-createDropdown("TypeFrontBar2", OogleCraftWeaponTypeFrontBar2Choices, 1500, 255, 125)
-createDropdown("TypeBackBar1", OogleCraftWeaponTypeBackBar1Choices, 1625, 255, 125)
-createDropdown("TypeBackBar2", OogleCraftWeaponTypeBackBar2Choices, 1750, 255, 125)
+createDropdown("TypeFrontBar1", OogleCraftWeaponTypeFrontBar1Choices, 1375, 245, 125)
+createDropdown("TypeFrontBar2", OogleCraftWeaponTypeFrontBar2Choices, 1500, 245, 125)
+createDropdown("TypeBackBar1", OogleCraftWeaponTypeBackBar1Choices, 1625, 245, 125)
+createDropdown("TypeBackBar2", OogleCraftWeaponTypeBackBar2Choices, 1750, 245, 125)
 
 ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 --- Weapon Trait Choices -----------------------------------------------------------------------------------------------------------------------------------------------------
@@ -70,10 +69,10 @@ local OogleCraftWeaponTraitFrontBar2Choices = {"[Front Bar #2]", "Powered", "Cha
 local OogleCraftWeaponTraitBackBar1Choices = {"[Back Bar #1]", "Powered", "Charged", "Precise", "Infused", "Defending", "Training", "Sharpened", "Decisive", "Nirnhoned"}
 local OogleCraftWeaponTraitBackBar2Choices = {"[Back Bar #2]", "Powered", "Charged", "Precise", "Infused", "Defending", "Training", "Sharpened", "Decisive", "Nirnhoned"}
 
-createDropdown("TraitFrontBar1", OogleCraftWeaponTraitFrontBar1Choices, 1375, 330, 125)
-createDropdown("TraitFrontBar2", OogleCraftWeaponTraitFrontBar2Choices, 1500, 330, 125)
-createDropdown("TraitBackBar1", OogleCraftWeaponTraitBackBar1Choices, 1625, 330, 125)
-createDropdown("TraitBackBar2", OogleCraftWeaponTraitBackBar2Choices, 1750, 330, 125)
+createDropdown("TraitFrontBar1", OogleCraftWeaponTraitFrontBar1Choices, 1375, 320, 125)
+createDropdown("TraitFrontBar2", OogleCraftWeaponTraitFrontBar2Choices, 1500, 320, 125)
+createDropdown("TraitBackBar1", OogleCraftWeaponTraitBackBar1Choices, 1625, 320, 125)
+createDropdown("TraitBackBar2", OogleCraftWeaponTraitBackBar2Choices, 1750, 320, 125)
 
 ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 --- Weapon Enchant Quality ----------------------------------------------------------------------------------------------------------------------------------------------------
@@ -81,7 +80,7 @@ createDropdown("TraitBackBar2", OogleCraftWeaponTraitBackBar2Choices, 1750, 330,
 
 local OogleCraftWeaponEnchantQualityChoices = {"[Weapon Enchant Quality]", "White", "Green", "Blue", "Purple", "Yellow"}
 
-createDropdown("EnchantQuality", OogleCraftWeaponEnchantQualityChoices, 1375, 405, 500)
+createDropdown("WeaponEnchantQuality", OogleCraftWeaponEnchantQualityChoices, 1375, 395, 500)
 
 ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 --- Weapon Enchant Choices ---------------------------------------------------------------------------------------------------------------------------------------------------
@@ -92,7 +91,7 @@ local OogleCraftWeaponEnchantFrontBar2Choices = {"[Front Bar #2]", "Absorb Healt
 local OogleCraftWeaponEnchantBackBar1Choices = {"[Back Bar #1]", "Absorb Health", "Absorb Magicka", "Absorb Stamina", "Decrease Health", "Poison", "Flame", "Frost", "Shock", "Foulness", "Crushing", "Weakening", "Weapon Damage", "Prismatic Onslaught"}
 local OogleCraftWeaponEnchantBackBar2Choices = {"[Back Bar #2]", "Absorb Health", "Absorb Magicka", "Absorb Stamina", "Decrease Health", "Poison", "Flame", "Frost", "Shock", "Foulness", "Crushing", "Weakening", "Weapon Damage", "Prismatic Onslaught"}
 
-createDropdown("EnchantFrontBar1", OogleCraftWeaponEnchantFrontBar1Choices, 1375, 430, 125)
-createDropdown("EnchantFrontBar2", OogleCraftWeaponEnchantFrontBar2Choices, 1500, 430, 125)
-createDropdown("EnchantBackBar1", OogleCraftWeaponEnchantBackBar1Choices, 1625, 430, 125)
-createDropdown("EnchantBackBar2", OogleCraftWeaponEnchantBackBar2Choices, 1750, 430, 125)
+createDropdown("EnchantFrontBar1", OogleCraftWeaponEnchantFrontBar1Choices, 1375, 420, 125)
+createDropdown("EnchantFrontBar2", OogleCraftWeaponEnchantFrontBar2Choices, 1500, 420, 125)
+createDropdown("EnchantBackBar1", OogleCraftWeaponEnchantBackBar1Choices, 1625, 420, 125)
+createDropdown("EnchantBackBar2", OogleCraftWeaponEnchantBackBar2Choices, 1750, 420, 125)
