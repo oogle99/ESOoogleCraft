c007a3798dae6db4d180e9ee6137a8e8c147cc56
oogle99 <158149418+oogle99@users.noreply.github.com>
2024-03-20 02:30:45 -0700

    Failed SavedVariables Execution

--- OogleCraftArmorDropUI.lua
+++ OogleCraftArmorDropUI.lua
@@ -6,7 +6,7 @@ _G.selectedItems = _G.selectedItems or {}
 local wm = WINDOW_MANAGER
 
 local function createDropdown(oogleCraftDropdownFrameName, choices, xOffset, yOffset, dropWide)
-    local dropdownContainer = wm:CreateControlFromVirtual("OogleCraftArmor" .. oogleCraftDropdownFrameName .. "Drop", OogleCraftMainPanel, "ScrollCombobox")
+    local dropdownContainer = wm:CreateControlFromVirtual("OogleCraftJewelry" .. oogleCraftDropdownFrameName .. "Drop", OogleCraftMainPanel, "ScrollCombobox")
     dropdownContainer:SetAnchor(TOPLEFT, OogleCraftMainPanel, TOPLEFT, xOffset + 7, yOffset)
     dropdownContainer:SetDimensions(125, 50)
     dropdownContainer:GetNamedChild("Dropdown"):SetWidth(dropWide)
@@ -18,22 +18,22 @@ local function createDropdown(oogleCraftDropdownFrameName, choices, xOffset, yOf
 
     local function OnItemSelect(_, choiceText, choice)
     
-    -- Check if the table for the current dropdown frame exists, if not, create it
-    _G.selectedItems[oogleCraftDropdownFrameName] = _G.selectedItems[oogleCraftDropdownFrameName] or {}
-    
-    -- Check if the selected item does not already exists for the current dropdown frame
-    for i, item in ipairs(_G.selectedItems[oogleCraftDropdownFrameName]) do
-        if item ~= choiceText then
-            -- Remove the existing item from the table
-            table.remove(_G.selectedItems[oogleCraftDropdownFrameName], i)
-            break -- Exit the loop once the item is removed
+        -- Check if the table for the current dropdown frame exists, if not, create it
+        _G.selectedItems[oogleCraftDropdownFrameName] = _G.selectedItems[oogleCraftDropdownFrameName] or {}
+        
+        -- Check if the selected item does not already exists for the current dropdown frame
+        for i, item in ipairs(_G.selectedItems[oogleCraftDropdownFrameName]) do
+            if item ~= choiceText or item == choiceText then
+                -- Remove the existing item from the table
+                table.remove(_G.selectedItems[oogleCraftDropdownFrameName], i)
+                break -- Exit the loop once the item is removed
+            end
         end
+        
+        -- Store the selected item in the table corresponding to the current dropdown frame
+        table.insert(_G.selectedItems[oogleCraftDropdownFrameName], choiceText)
     end
     
-    -- Store the selected item in the table corresponding to the current dropdown frame
-    table.insert(_G.selectedItems[oogleCraftDropdownFrameName], choiceText)
-    end
-
     for i=1,#choices do
         local entry = dropdown:CreateItemEntry(choices[i], OnItemSelect) -- [, OnItemSelect]
         dropdown:AddItem(entry)
@@ -41,103 +41,53 @@ local function createDropdown(oogleCraftDropdownFrameName, choices, xOffset, yOf
 end
 
 ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
---- Armor Quality Choices ----------------------------------------------------------------------------------------------------------------------------------------------------
+--- Jewelry Quality Choices ----------------------------------------------------------------------------------------------------------------------------------------------------
 ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-local OogleCraftArmorSetQualityChoices = {"[Armor Quality]", "White", "Green", "Blue", "Purple", "Yellow", "Orange"}
+local OogleCraftJewelrySetQualityChoices = {"[Jewelry Quality]", "White", "Green", "Blue", "Purple", "Yellow", "Orange"}
 
-createDropdown("ArmorSetQuality", OogleCraftArmorSetQualityChoices, 0, 145, 1000)
-
-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
---- Armor Set Choices --------------------------------------------------------------------------------------------------------------------------------------------------------
-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-local OogleCraftArmorSetShieldChoices = {"[Shield]", "Set #1", "Set #2", "Set #3"}
-local OogleCraftArmorSetChestChoices = {"[Chest]", "Set #1", "Set #2", "Set #3"}
-local OogleCraftArmorSetFeetChoices = {"[Feet]", "Set #1", "Set #2", "Set #3"}
-local OogleCraftArmorSetHandsChoices = {"[Hands]", "Set #1", "Set #2", "Set #3"}
-local OogleCraftArmorSetLegsChoices = {"[Legs]", "Set #1", "Set #2", "Set #3"}
-local OogleCraftArmorSetWaistChoices = {"[Waist]", "Set #1", "Set #2", "Set #3"}
-local OogleCraftArmorSetHeadChoices = {"[Head]", "Set #1", "Set #2", "Set #3"}
-local OogleCraftArmorSetShouldersChoices = {"[Shoulders]", "Set #1", "Set #2", "Set #3"}
-
-createDropdown("SetShield", OogleCraftArmorSetShieldChoices, 0, 170, 125)
-createDropdown("SetChest", OogleCraftArmorSetChestChoices, 125, 170, 125)
-createDropdown("SetFeet", OogleCraftArmorSetFeetChoices, 250, 170, 125)
-createDropdown("SetHands", OogleCraftArmorSetHandsChoices, 375, 170, 125)
-createDropdown("SetLegs", OogleCraftArmorSetLegsChoices, 500, 170, 125)
-createDropdown("SetWaist", OogleCraftArmorSetWaistChoices, 625, 170, 125)
-createDropdown("SetHead", OogleCraftArmorSetHeadChoices, 750, 170, 125)
-createDropdown("SetShoulders", OogleCraftArmorSetShouldersChoices, 875, 170, 125)
+createDropdown("JewelrySetQuality", OogleCraftJewelrySetQualityChoices, 640, 145, 240)
 
 ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
---- Armor Weight Choices -----------------------------------------------------------------------------------------------------------------------------------------------------
+--- Jewelry Set Choices ------------------------------------------------------------------------------------------------------------------------------------------------------
 ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-local OogleCraftArmorWeightChestChoices = {"[Chest]", "Light", "Medium", "Heavy"}
-local OogleCraftArmorWeightFeetChoices = {"[Feet]", "Light", "Medium", "Heavy"}
-local OogleCraftArmorWeightHandsChoices = {"[Hands]", "Light", "Medium", "Heavy"}
-local OogleCraftArmorWeightLegsChoices = {"[Legs]", "Light", "Medium", "Heavy"}
-local OogleCraftArmorWeightWaistChoices = {"[Waist]", "Light", "Medium", "Heavy"}
-local OogleCraftArmorWeightHeadChoices = {"[Head]", "Light", "Medium", "Heavy"}
-local OogleCraftArmorWeightShouldersChoices = {"[Shoulders]", "Light", "Medium", "Heavy"}
+local OogleCraftJewelrySetRing1Choices = {"[Set#]", "Set #1", "Set #2", "Set #3"}
+local OogleCraftJewelrySetRing2Choices = {"[Set#]", "Set #1", "Set #2", "Set #3"}
+local OogleCraftJewelrySetNecklaceChoices = {"[Set#]", "Set #1", "Set #2", "Set #3"}
 
-createDropdown("WeightChest", OogleCraftArmorWeightChestChoices, 125, 245, 125)
-createDropdown("WeightFeet", OogleCraftArmorWeightFeetChoices, 250, 245, 125)
-createDropdown("WeightHands", OogleCraftArmorWeightHandsChoices, 375, 245, 125)
-createDropdown("WeightLegs", OogleCraftArmorWeightLegsChoices, 500, 245, 125)
-createDropdown("WeightWaist", OogleCraftArmorWeightWaistChoices, 625, 245, 125)
-createDropdown("WeightHead", OogleCraftArmorWeightHeadChoices, 750, 245, 125)
-createDropdown("WeightShoulders", OogleCraftArmorWeightShouldersChoices, 875, 245, 125)
+createDropdown("SetRing1", OogleCraftJewelrySetRing1Choices, 640, 170, 80)
+createDropdown("SetRing2", OogleCraftJewelrySetRing2Choices, 720, 170, 80)
+createDropdown("SetNecklace", OogleCraftJewelrySetNecklaceChoices, 800, 170, 80)
 
 ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
---- Armor Trait Choices -----------------------------------------------------------------------------------------------------------------------------------------------------
+--- Jewelry Trait Choices ----------------------------------------------------------------------------------------------------------------------------------------------------
 ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-local OogleCraftArmorTraitShieldChoices = {"[Shield]", "Sturdy", "Impenetrable", "Reinforced", "Well-fitted", "Training", "Infused", "Invigorating", "Divines", "Nirnhoned"}
-local OogleCraftArmorTraitChestChoices = {"[Chest]", "Sturdy", "Impenetrable", "Reinforced", "Well-fitted", "Training", "Infused", "Invigorating", "Divines", "Nirnhoned"}
-local OogleCraftArmorTraitFeetChoices = {"[Feet]", "Sturdy", "Impenetrable", "Reinforced", "Well-fitted", "Training", "Infused", "Invigorating", "Divines", "Nirnhoned"}
-local OogleCraftArmorTraitHandsChoices = {"[Hands]", "Sturdy", "Impenetrable", "Reinforced", "Well-fitted", "Training", "Infused", "Invigorating", "Divines", "Nirnhoned"}
-local OogleCraftArmorTraitLegsChoices = {"[Legs]", "Sturdy", "Impenetrable", "Reinforced", "Well-fitted", "Training", "Infused", "Invigorating", "Divines", "Nirnhoned"}
-local OogleCraftArmorTraitWaistChoices = {"[Waist]", "Sturdy", "Impenetrable", "Reinforced", "Well-fitted", "Training", "Infused", "Invigorating", "Divines", "Nirnhoned"}
-local OogleCraftArmorTraitHeadChoices = {"[Head]", "Sturdy", "Impenetrable", "Reinforced", "Well-fitted", "Training", "Infused", "Invigorating", "Divines", "Nirnhoned"}
-local OogleCraftArmorTraitShouldersChoices = {"[Shoulders]", "Sturdy", "Impenetrable", "Reinforced", "Well-fitted", "Training", "Infused", "Invigorating", "Divines", "Nirnhoned"}
+local OogleCraftJewelryTraitRing1Choices = {"[Trait]", "Arcane", "Healthy", "Robust", "Triune", "Infused", "Protective", "Swift", "Harmony", "Bloodthirsty"}
+local OogleCraftJewelryTraitRing2Choices = {"[Trait]", "Arcane", "Healthy", "Robust", "Triune", "Infused", "Protective", "Swift", "Harmony", "Bloodthirsty"}
+local OogleCraftJewelryTraitNecklaceChoices = {"[Trait]", "Arcane", "Healthy", "Robust", "Triune", "Infused", "Protective", "Swift", "Harmony", "Bloodthirsty"}
 
-createDropdown("TraitShield", OogleCraftArmorTraitShieldChoices, 0, 320, 125)
-createDropdown("TraitChest", OogleCraftArmorTraitChestChoices, 125, 320, 125)
-createDropdown("TraitFeet", OogleCraftArmorTraitFeetChoices, 250, 320, 125)
-createDropdown("TraitHands", OogleCraftArmorTraitHandsChoices, 375, 320, 125)
-createDropdown("TraitLegs", OogleCraftArmorTraitLegsChoices, 500, 320, 125)
-createDropdown("TraitWaist", OogleCraftArmorTraitWaistChoices, 625, 320, 125)
-createDropdown("TraitHead", OogleCraftArmorTraitHeadChoices, 750, 320, 125)
-createDropdown("TraitShoulders", OogleCraftArmorTraitShouldersChoices, 875, 320, 125)
+createDropdown("TraitRing1", OogleCraftJewelryTraitRing1Choices, 640, 320, 80)
+createDropdown("TraitRing2", OogleCraftJewelryTraitRing2Choices, 720, 320, 80)
+createDropdown("TraitNecklace", OogleCraftJewelryTraitNecklaceChoices, 800, 320, 80)
 
 ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
---- Armor Enchant Quality ----------------------------------------------------------------------------------------------------------------------------------------------------
+--- Jewelry Enchant Quality ----------------------------------------------------------------------------------------------------------------------------------------------------
 ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-local OogleCraftArmorEnchantQualityChoices = {"[Armor Enchant Quality]", "White", "Green", "Blue", "Purple", "Yellow"}
+local OogleCraftJewelryEnchantQualityChoices = {"[Jewelry Enchant Quality]", "White", "Green", "Blue", "Purple", "Yellow"}
 
-createDropdown("ArmorEnchantQuality", OogleCraftArmorEnchantQualityChoices, 0, 395, 1000)
+createDropdown("JewelryEnchantQuality", OogleCraftJewelryEnchantQualityChoices, 640, 395, 240)
 
 ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
---- Armor Enchant Choices ----------------------------------------------------------------------------------------------------------------------------------------------------
+--- Jewelry Enchant Choices --------------------------------------------------------------------------------------------------------------------------------------------------
 ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-local OogleCraftArmorEnchantShieldChoices = {"[Shield]", "Health", "Magicka", "Stamina", "Prismatic Defense"}
-local OogleCraftArmorEnchantChestChoices = {"[Chest]", "Health", "Magicka", "Stamina", "Prismatic Defense"}
-local OogleCraftArmorEnchantFeetChoices = {"[Feet]", "Health", "Magicka", "Stamina", "Prismatic Defense"}
-local OogleCraftArmorEnchantHandsChoices = {"[Hands]", "Health", "Magicka", "Stamina", "Prismatic Defense"}
-local OogleCraftArmorEnchantLegsChoices = {"[Legs]", "Health", "Magicka", "Stamina", "Prismatic Defense"}
-local OogleCraftArmorEnchantWaistChoices = {"[Waist]", "Health", "Magicka", "Stamina", "Prismatic Defense"}
-local OogleCraftArmorEnchantHeadChoices = {"[Head]", "Health", "Magicka", "Stamina", "Prismatic Defense"}
-local OogleCraftArmorEnchantShouldersChoices = {"[Shoulders]", "Health", "Magicka", "Stamina", "Prismatic Defense"}
+local OogleCraftJewelryEnchantRing1Choices = {"[Ench.]", "None", "Health Recovery", "Reduce Spell Cost", "Magicka Recovery", "Reduce Feat Cost", "Stamina Recovery", "Poison Resist", "Flame Resist", "Frost Resist", "Shock Resist", "Disease Resist", "Potion Speed", "Potion Boost", "Decrease Physical Harm", "Increase Physical Harm", "Decrease Spell Harm", "Increase Magical Harm", "Shielding", "Bashing", "Reduce Skill Cost", "Prismatic Recovery"}
+local OogleCraftJewelryEnchantRing2Choices = {"[Ench.]", "None", "Health Recovery", "Reduce Spell Cost", "Magicka Recovery", "Reduce Feat Cost", "Stamina Recovery", "Poison Resist", "Flame Resist", "Frost Resist", "Shock Resist", "Disease Resist", "Potion Speed", "Potion Boost", "Decrease Physical Harm", "Increase Physical Harm", "Decrease Spell Harm", "Increase Magical Harm", "Shielding", "Bashing", "Reduce Skill Cost", "Prismatic Recovery"}
+local OogleCraftJewelryEnchantNecklaceChoices = {"[Ench.]", "None", "Health Recovery", "Reduce Spell Cost", "Magicka Recovery", "Reduce Feat Cost", "Stamina Recovery", "Poison Resist", "Flame Resist", "Frost Resist", "Shock Resist", "Disease Resist", "Potion Speed", "Potion Boost", "Decrease Physical Harm", "Increase Physical Harm", "Decrease Spell Harm", "Increase Magical Harm", "Shielding", "Bashing", "Reduce Skill Cost", "Prismatic Recovery"}
 
-createDropdown("EnchantShield", OogleCraftArmorEnchantShieldChoices, 0, 420, 125)
-createDropdown("EnchantChest", OogleCraftArmorEnchantChestChoices, 125, 420, 125)
-createDropdown("EnchantFeet", OogleCraftArmorEnchantFeetChoices, 250, 420, 125)
-createDropdown("EnchantHands", OogleCraftArmorEnchantHandsChoices, 375, 420, 125)
-createDropdown("EnchantLegs", OogleCraftArmorEnchantLegsChoices, 500, 420, 125)
-createDropdown("EnchantWaist", OogleCraftArmorEnchantWaistChoices, 625, 420, 125)
-createDropdown("EnchantHead", OogleCraftArmorEnchantHeadChoices, 750, 420, 125)
-createDropdown("EnchantShoulders", OogleCraftArmorEnchantShouldersChoices, 875, 420, 125)
+createDropdown("EnchantRing1", OogleCraftJewelryEnchantRing1Choices, 640, 420, 80)
+createDropdown("EnchantRing2", OogleCraftJewelryEnchantRing2Choices, 720, 420, 80)
+createDropdown("EnchantNecklace", OogleCraftJewelryEnchantNecklaceChoices, 800, 420, 80)
